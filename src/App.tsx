0ff361fbe2cb6d1b3c8e977eb3411ea59d76b848
Williams Balogun <williamsdev.official@gmail.com>
2023-04-17 00:48:32 +0100

    feat: Router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { ReactElement } from 'react';
+import { createBrowserRouter as browser } from 'react-router-dom';
+import { useAppSelector } from './hooks';
 
-const App = (): ReactElement => {
-  return <div></div>;
+const App = () => {
+  const { loggedin } = useAppSelector((state) => state.user);
+  return browser([
+    {
+      path: '/',
+      element: loggedin ? '' : '',
+    },
+  ]);
 };
 
 export default App;
